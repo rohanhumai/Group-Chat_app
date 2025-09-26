464ce6d96d8b93eb9f8d5cdaaf01f8c1f98b5f40
Rohan Coder <116200861+rohanhumai@users.noreply.github.com>
2025-09-26 21:44:26 +0530

    index.js

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+
+// socket.io with CORS setup
+const io = new Server(server, {
+    cors: {
+        origin: "https://group-chat-app-axqm.onrender.com/", // later replace * with your frontend deployed URL
+        methods: ["GET", "POST"],
+    },
+});
 
 // io server
 io.on("connection", (socket) => {
@@ -25,4 +32,6 @@ app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 
-server.listen(9000, () => console.log(`Server started on: http://localhost:9000`));
+// ✅ use Render's port or default to 9000 locally
+const PORT = process.env.PORT || 9000;
+server.listen(PORT, () => console.log(`Server started on: http://localhost:${PORT}`));
